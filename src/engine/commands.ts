c29b45d102bbf2195dad7539a4e04e385a1f65b5
Oliver White <g.g.otto15@gmail.com>
2026-07-05 23:54:21 -0700

    tiny change

--- src/engine/commands.ts
+++ src/engine/commands.ts
@@ -256,7 +256,7 @@ const commands: CommandDefinition[] = [
 		aliases: ["examine", "inspect", "look at", "x"],
 		description: "Examine something more closely.",
 		connectors: ["in front of"],
-		run: ({gameState, parsed}) => {
+		run: ({world, gameState, parsed}) => {
 			const targetText = parsed.targetText;
 
 			if (!targetText) {
